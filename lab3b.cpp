295c086611ce870e4ed2e6feb38bbba9cbe5cc0b
mtack81 <mtack81@live.com>
2026-09-03 12:59:35 -0400

    Finished Box Office program

---
+++ lab3b.cpp
@@ -0,0 +1,41 @@
+// box office program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string movie;
+	int adults;
+	int seniors;
+	int children;
+	float prad = 11.50;
+	float prsen = 10.00;
+	float prchi = 8.50;
+	
+	cout<< "Box Office Program"<< endl;
+	cout<< endl;
+	cout<< "What is the name of the movie? ";
+	cin>> movie;
+	cout<< "How many adults attended? ";
+	cin>> adults;
+	cout<< "How many seniors attended? ";
+	cin>> seniors;
+	cout<< "How many children attended? ";
+	cin>> children;
+	cout<< endl;
+		
+	float gross = (adults*prad)+(seniors*prsen)+(children*prchi);
+	float net = gross*.35;
+	float dist = gross*.65;
+	
+	cout<< "Movie Name:                   "<< "\"" << movie << "\"" << endl;
+	cout<< "Adult Tickets Sold:             "<< adults<< endl;
+	cout<< "Senior Tickets Sold:            "<< seniors<< endl;
+	cout<< "Child Tickets Sold:             "<< children<< endl;
+	cout<< "Gross Box Office Profit:      $ "<< gross<< endl;
+	cout<< "Net Box Office Profit:        $ "<< net<< endl;
+	cout<< "Amount Paid to Distributor:   $ "<< dist<< endl;
+	
+	return 0;
+}
