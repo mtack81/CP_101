9dcce0bfba906e272fc1aea9664abf38d3d045dd
mtack81 <mtack81@live.com>
2026-09-16 12:50:56 -0400

    added some more stuff for the lab4.2

--- lab4.2.cpp
+++ lab4.2.cpp
@@ -14,20 +14,19 @@ int main()
 	float ln;
 	
   
-cout << "Box Volume calculator";
-cout << endl; //added space between title and questions for easier reading
-cout << "What is the height of the box? "; //moved question mark and added space to make for easier reading
-cin >> ht; //<< was used instead of >> for cin
-cout << "What is the width of the box? "; //moved question mark and added space to make for easier reading
-cin >> wd; //<< was used instead of >>
-cout << "What is the length of the box? "; //moved question mark and added space to make for easier reading and wrong > was used
-cin >> ln; //changed to ln which is the declared variable for length
-cout << endl; //changed this line because i think it was meant to be a blank line using the \n
+	cout << "Box Volume calculator" << endl; //added endl on here to make it go to the next line 
+	cout << endl; //added space between title and questions for easier reading
+	cout << "What is the height of the box? "; //moved question mark and added space to make for easier reading
+	cin >> ht; //<< was used instead of >> for cin
+	cout << "What is the width of the box? "; //moved question mark and added space to make for easier reading
+	cin >> wd; //<< was used instead of >>
+	cout << "What is the length of the box? "; //moved question mark and added space to make for easier reading and wrong > was used
+	cin >> ln; //changed to ln which is the declared variable for length
+	cout << endl; //changed this line because i think it was meant to be a blank line using the \n
 
-float vol = ht*wd*ln; //moved here due to needing the input from the cin
+	float vol = ht*wd*ln; //moved here due to needing the input from the cin
 
-cout << "The Volume of the box is " << vol; //removed a bunch of stuff used with stdio.h not iostream and then added the variable for output
+	cout << "The Volume of the box is " << vol; //removed a bunch of stuff used with stdio.h not iostream and then added the variable for output
 
-
-return 0;
+	return 0;
 }
