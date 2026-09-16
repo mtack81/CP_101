566512d561a6d6fc03aa43e678153e811a626941
mtack81 <mtack81@live.com>
2026-09-16 10:46:39 -0400

    finished up the lab4.1c

--- lab4.1c.cpp
+++ lab4.1c.cpp
@@ -11,12 +11,15 @@ int main()
 	float interest;
 	int years;
 	
+	cout << "Monthly vs Bi-Monthly Payment Calculation" << endl;
+	cout << endl;
 	cout << "What is the loan amount? ";
 	cin >> loan;
 	cout << "What is the annual interest rate (enter 1% as 1) ? ";
 	cin >> interest;
 	cout << "What is the number of years for the loan? ";
 	cin >> years;
+	cout << endl;
 	
 	float monthrate = (interest/100)/12;
 	int numpay = years * 12;
@@ -27,16 +30,23 @@ int main()
 	float bigrowthfactor = pow(1 + birate, binumpay);
 	float biemi = loan * (birate*bigrowthfactor)/(bigrowthfactor - 1);
 	float totalbi = biemi*2;
-		
+	float savings = (emi*numpay)-(biemi*binumpay);
+			
 	cout << fixed << setprecision(2);
 	
-	cout << setw(20) << left << "Loan Amount" << ": $" << setw(10) << right << loan << endl;
-	cout << setw(20) << left << "Annual Interest Rate" << ": " << setw(11) << right << interest << "%"<< endl;
-	cout << setw(20) << left << "Monthly Payment" << ": $" << setw(10) << right << emi << endl;
-	cout << setw(20) << left << "Total Paid using Monthly" << ": $" << setw(10) << right << emi*numpay << endl;
-	cout << setw(20) << left << "Interest Paid using Monthly" << ": $" << setw(10) << right << (emi*numpay)-loan << endl;
-	cout << setw(20) << left << "BiMonthly Payment" << ": $" << setw(10) << right << biemi << endl;
-	cout << setw(20) << left << "Total BiMonthly/Monthly Cost" << ": $" << setw(10) << right << totalbi << endl;
+	cout << setw(30) << left << "Loan Amount" << ": $" << setw(10) << right << loan << endl;
+	cout << setw(30) << left << "Annual Interest Rate" << ": " << setw(11) << right << interest << "%"<< endl;
+	cout << endl;
+	cout << setw(30) << left << "Monthly Payment" << ": $" << setw(10) << right << emi << endl;
+	cout << setw(30) << left << "Total Paid using Monthly" << ": $" << setw(10) << right << emi*numpay << endl;
+	cout << setw(30) << left << "Interest Paid using Monthly" << ": $" << setw(10) << right << (emi*numpay)-loan << endl;
+	cout << endl;
+	cout << setw(30) << left << "BiMonthly Payment" << ": $" << setw(10) << right << biemi << endl;
+	cout << setw(30) << left << "Total BiMonthly/Monthly Cost" << ": $" << setw(10) << right << totalbi << endl;
+	cout << setw(30) << left << "Total Paid using BiMonthly" << ": $" << setw(10) << right << biemi*binumpay << endl;
+	cout << setw(30) << left << "Interest Paid using BiMonthly" << ": $" << setw(10) << right << (biemi*binumpay)-loan << endl;
+	cout << endl;
+	cout << setw(30) << left << "Savings when using BiMonthly" << ": $" << setw(10) << right << savings << endl;
 	
 	return 0;
 	
