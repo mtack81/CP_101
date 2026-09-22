128666ba11e3319729306b091c63dbb167606798
mtack81 <mtack81@live.com>
2026-09-22 15:36:52 -0400

    lab4.1b - rain/snow gauge calc, iostream version

--- lab4.1b.cpp
+++ lab4.1b.cpp
@@ -1,5 +1,5 @@
 // Michael Tackett k0475835 fall semester 2026
-
+// Rain/snow gauge calc - iostream version of lab4.1a (same math, cin/cout only)
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -11,7 +11,10 @@ int main()
       string town;
       float rainAM;
       float rainPM;
-            
+      
+// getline(cin, town) - the string version (no size limit needed, unlike char arrays)
+// vs cin.getline(town, 64) used in lab4.1a for a char array, which DOES need a size  
+         
       cout<< "Winter 26/27 Rain/Snow Gauge"<< endl;
       cout<< endl;
       cout<< "What is the name of the town? ";
@@ -25,6 +28,9 @@ int main()
       float totalrain = rainAM + rainPM;
       float snowfall = totalrain * 11.75;
       
+// fixed + setprecision(3) set once here, apply to ALL cout output after this line
+// until changed - matches the %.3f used per-value in the printf version of lab4.1a 
+     
       cout << fixed << setprecision(3);
       cout<< "In "<< town<< " today there was "<< rainAM<< " inches in the morning and "<< "\n"<< rainPM<< " inches in the afternoon/evening for a total of "<< totalrain
           << " inches of rain."<< "\n"<< "This would be expected to accumulate to about "<< snowfall<< " inches of snowfall.";
